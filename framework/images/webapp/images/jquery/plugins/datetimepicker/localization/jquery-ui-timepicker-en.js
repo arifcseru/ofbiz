1a15b9a123fa4ea7efb2ddec762fc5198df999c2
zfall <fall.zoltan@gmail.com>
2013-03-24 14:54:47 +0100

    Fixed original javascript 404 issue. 2

---
+++ framework/images/webapp/images/jquery/plugins/datetimepicker/localization/jquery-ui-timepicker-en.js
@@ -0,0 +1,17 @@
+/* English translation for the jQuery Timepicker Addon */
+/* Written by Zoltan Fall */
+(function($) {
+	$.timepicker.regional['en'] = {
+		timeOnlyTitle: 'Choose time',
+		timeText: 'Time',
+		hourText: 'Hour',
+		minuteText: 'Minute',
+		secondText: 'Second',
+		timezoneText: 'Timezone',
+		currentText: 'Current',
+		closeText: 'Close',
+		timeFormat: 'hh:mm tt',
+		ampm: true
+	};
+	$.timepicker.setDefaults($.timepicker.regional['en']);
+})(jQuery);
